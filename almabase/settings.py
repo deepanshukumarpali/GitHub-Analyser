7d4847a5aed508a6cb3ad9cdd23452f667c6c8c2
Deepanshu Kumar Pali <deepanshukumarpali7@gmail.com>
2020-11-08 16:20:37 +0530

    host allowed

--- almabase/settings.py
+++ almabase/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '3^q5a=mdid5@0ri03gcdrn-08!9^1-wkey#l5l4rh=(qo^-hq8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
